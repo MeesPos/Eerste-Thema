a275f0910f68ae3f9be3a93b6119fe0c9701a427
MeesPos <meespostma_@hotmail.com>
2020-06-12 12:03:48 +0200

    Klaar t/m video 12

--- functions.php
+++ functions.php
@@ -1,20 +1,28 @@
 <?php
-// Laad stylesheets in
-function loadStylesheets() {
+// Laad stylesheets in en scripts
+function loadCSSandJS() {
     wp_enqueue_style( 'style', get_stylesheet_uri() );
     wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.css' );
+    wp_enqueue_script('bootstrapjs', get_template_directory_uri() . '/js/bootstrap.min.js', array ( 'jquery' ), 1.1, true);
 }
 
-add_action( 'init', 'loadStylesheets');
+add_action( 'init', 'loadCSSandJS');
 
 // Menu registreren
 function register_menus() {
     register_nav_menus(
         array(
-            'main-menu' => __( 'Main menu' ),
-            'extra-menu' => __( 'Extra menu' )
+            'main-menu' => __( 'Main menu' )
         )
     );
 }
 
 add_action( 'init', 'register_menus' );
+
+/**
+ * Register Custom Navigation Walker
+ */
+function register_navwalker(){
+	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'init', 'register_navwalker' );
--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes() ?>>
+
 <head>
     <meta charset="<?php bloginfo('charset') ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,15 +8,32 @@
     <title><?php bloginfo('name') ?></title>
     <?php wp_head(); ?>
 </head>
+
 <body>
-<nav>
-    <?php wp_nav_menu( 
-        array(
-            'theme_loction' => 'main-menu'
-        ) 
-    ); ?>
-</nav>
-<div class="jumbotron">
-    <h1 class="display-4"><a href="<?php echo home_url(); ?>"><?php bloginfo('name') ?></a></h1>
-    <p class="lead"><?php bloginfo('description') ?></p>
-</div>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-primary" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#">Mees Postma</a>
+            <?php
+            wp_nav_menu(array(
+                'theme_location'    => 'main-menu',
+                'depth'             => 2,
+                'container'         => 'div',
+                'container_class'   => 'collapse navbar-collapse',
+                'container_id'      => 'bs-example-navbar-collapse-1',
+                'menu_class'        => 'nav navbar-nav',
+                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                'walker'            => new WP_Bootstrap_Navwalker(),
+            ));
+            ?>
+        </div>
+    </nav>
+
+    <div class="jumbotron" style="background-image: url(<?php echo get_template_directory_uri() . '/img/header.jpg' ?>);">
+        <h1 class="display-4"><a href="<?php echo home_url(); ?>"><?php bloginfo('name') ?></a></h1>
+        <p class="lead"><?php bloginfo('description') ?></p>
+    </div>
